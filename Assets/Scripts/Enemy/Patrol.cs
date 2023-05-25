d9170a707a3d4bcf33a6fd0f6527c3443ec60a56
Drew White <54377401+drewthemain@users.noreply.github.com>
2023-05-24 23:21:56 -0400

    Cut out Patrol, added Target prefab, initial work on Tracer enemy

--- Assets/Scripts/Enemy/Patrol.cs
+++ Assets/Scripts/Enemy/Patrol.cs
@@ -28,7 +28,7 @@ public class Patrol : Enemy
     [Tooltip("The speed of the patrol search sweeping movement")]
     [SerializeField] private float _sweepSpeed = 0.5f;
 
-    // The target of this enemy
+    // The target of this enemies target
     private Transform _target;
 
     // Is the enemy currently moving?
